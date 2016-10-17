385dcf0d02bc5381517ea24b063bd640a9c8dc38
Key Hui <key.hui@gmail.com>
2016-10-18 00:16:00 +0800

    no message

---
+++ DSGetLite/DSGetLite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DSGetLite
+//
+//  Created by Hui Key on 18/10/2016.
+//  Copyright © 2016 Key Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DSGetLite/DSGetLiteTests/DSGetLiteTests.swift
@@ -0,0 +1,36 @@
+//
+//  DSGetLiteTests.swift
+//  DSGetLiteTests
+//
+//  Created by Hui Key on 18/10/2016.
+//  Copyright © 2016 Key Hui. All rights reserved.
+//
+
+import XCTest
+@testable import DSGetLite
+
+class DSGetLiteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
